5b756e9f3ccfcc67b37cdbe8a46c47ad411beb2d
aashik <aashikdevelop@gmail.com>
2024-01-28 06:20:48 +0530

    Theme Switcher and Mobile Navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,27 +3,26 @@ import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 const inter = Inter({
   subsets: ["latin"],
-  weight:['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable:'--font-inter'
-})
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 
 const spaceGrotesk = Inter({
   subsets: ["latin"],
-  weight:['300', '400', '500', '600', '700'],
-  variable:'--font-spaceGrotesk'
-})
-
-
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
   title: "Dev OverFlow",
   description: "about the app....",
-  icons:{
-    icon:'/assets/images/site-logo.svg'
-  }
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -32,14 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{
-      elements:{
-        formButtonPrimary:'primary-gradient',
-        footerActionLink:'primary-text-gradient hover:text-primary-500',
-      }
-    }}>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body className={ `${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <ThemeProvider>{children}</ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
