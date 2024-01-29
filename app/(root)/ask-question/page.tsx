63ab14e6e25e58282cd7b361aaec9476308b3082
aashik <aashikdevelop@gmail.com>
2024-01-30 03:40:47 +0530

    Question Form and Validations

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,16 @@
+import Question from "@/components/forms/Question";
 import React from "react";
 
 const AskQuestion = () => {
-  return <h1>AskQuestion</h1>;
+  return (
+    <div className="h1-bold text-dark100_light900">
+      <h1>Ask a Question</h1>
+
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
