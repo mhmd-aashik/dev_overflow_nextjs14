3c8bfd128d995894fb11db4487d7df5273d6d23e
aashik <aashikdevelop@gmail.com>
2024-02-02 04:46:13 +0530

    bugs fixing

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -11,7 +11,6 @@ import Link from "next/link";
 
 export default async function Home() {
   const result = await getQuestions({});
-  console.log(result.questions);
 
   return (
     <>
--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 const AskQuestion = async () => {
   const { userId } = auth();
-  console.log(userId);
+
 
   if (!userId) redirect("/sign-in");
 
